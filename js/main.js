01dd18c653309153f08b76274f47aca91f4f9d34
Yesid <yesidneo@gmail.com>
2021-08-14 15:47:01 -0500

    jpg_new version: first commit

---
+++ js/main.js
@@ -0,0 +1,489 @@
+
+
+
+ document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+
+
+
+ // svg logo section videoclips
+let animation = bodymovin.loadAnimation({
+    container: document.getElementById('log'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'logoj.json'
+
+});
+
+let animationori = bodymovin.loadAnimation({
+    container: document.getElementById('logi'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'logoj.json'
+
+});
+
+let animationa = bodymovin.loadAnimation({
+  container: document.getElementById('logia'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+let animationb = bodymovin.loadAnimation({
+  container: document.getElementById('logib'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+let animationc = bodymovin.loadAnimation({
+  container: document.getElementById('logic'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+let animationd = bodymovin.loadAnimation({
+  container: document.getElementById('logid'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+let animatione = bodymovin.loadAnimation({
+  container: document.getElementById('logie'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+let animationf = bodymovin.loadAnimation({
+  container: document.getElementById('logif'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+let animationlogfoot = bodymovin.loadAnimation({
+  container: document.getElementById('logf'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+
+// Modals
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.a');
+  var instances = M.Modal.init(elems);
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.b');
+  var instances = M.Modal.init(elems);
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.c');
+  var instances = M.Modal.init(elems);
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.e');
+  var instances = M.Modal.init(elems);
+});
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.f');
+  var instances = M.Modal.init(elems);
+});
+
+
+
+
+
+
+
+ // svg logo section  STREAMING
+ let stanimationa = bodymovin.loadAnimation({
+  container: document.getElementById('stlogia'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+let stanimationb = bodymovin.loadAnimation({
+  container: document.getElementById('stlogib'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+
+let stanimationc = bodymovin.loadAnimation({
+  container: document.getElementById('stlogic'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+let stanimationd = bodymovin.loadAnimation({
+  container: document.getElementById('stlogid'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+
+
+ // Modals section STREAMING 
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sta');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.stb');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.stc');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.std');
+  var instances = M.Modal.init(elems);
+});
+
+
+
+
+
+
+
+
+
+ // svg logo section PUBLICIDAD
+ let puanimationa = bodymovin.loadAnimation({
+  container: document.getElementById('pulogia'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+let puanimationb = bodymovin.loadAnimation({
+  container: document.getElementById('pulogib'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+
+let puanimationc = bodymovin.loadAnimation({
+  container: document.getElementById('pulogic'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+let puanimationd = bodymovin.loadAnimation({
+  container: document.getElementById('pulogid'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+
+
+ // Modals section PUBLICIDAD
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.pua');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.pub');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.puc');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.pud');
+  var instances = M.Modal.init(elems);
+});
+
+
+
+
+
+
+
+
+
+
+ // svg logo section SHOWS
+ let shanimationa = bodymovin.loadAnimation({
+  container: document.getElementById('shlogia'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+let shanimationb = bodymovin.loadAnimation({
+  container: document.getElementById('shlogib'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+
+let shanimationc = bodymovin.loadAnimation({
+  container: document.getElementById('shlogic'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+let shanimationd = bodymovin.loadAnimation({
+  container: document.getElementById('shlogid'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'logoj.json'
+
+});
+
+
+
+ // Modals section SHOWS
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sha');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.shb');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.shc');
+  var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.shd');
+  var instances = M.Modal.init(elems);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+//headroom.js--
+
+var myElement = document.querySelector("header");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement);
+// initialise
+headroom.init();
+
+//end headroom.js
+
+  
+
+
+
+// AOS ANIMATE
+
+
+
+AOS.init();
+
+
+
+
+
+
+
+
+//Relax js parallax
+
+var rellax = new Rellax('.rellax', {
+    callback: function(position) {
+
+    }
+  });
+
+  //end relax js
+
+
+
+  
+
+ // let test = document.getElementById('bota');
+ // test.onclick = showa;
+
+
+
+
+/*  OPTION Menuworks Funtions
+  function showa() {
+
+      document.getElementsById('works').style.display='block';
+      document.getElementById('botb').style.display='none';
+      document.getElementById('botc').style.display='none';
+
+    
+  }
+
+  function showb() {
+
+    document.getElementById('works').style.display='none';
+    document.getElementById('botb').style.display='block';
+    document.getElementById('botc').style.display='none';
+
+  
+}
+  
+function showc() {
+
+  document.getElementById('works').style.display='none';
+  document.getElementById('botb').style.display='none';
+  document.getElementById('botc').style.display='block';
+
+
+}
+
+  
+function showd() {
+
+  document.getElementById('works').style.display='none';
+  document.getElementById('botb').style.display='none';
+  document.getElementById('botc').style.display='block';
+
+
+}
+
+*/ 
+/*
+function showa(){
+
+  alert("HEllo WOrld");
+
+  let sw = document.getElementById('works');
+  sw = 
+}
+*/
+
+function showa() {
+
+  document.getElementById('bota').style.display='block';
+  document.getElementById('botb').style.display='none';
+  document.getElementById('botc').style.display='none';
+  document.getElementById('botd').style.display='none';
+
+
+
+}
+
+function showb() {
+
+document.getElementById('bota').style.display='none';
+document.getElementById('botb').style.display='block';
+document.getElementById('botc').style.display='none';
+document.getElementById('botd').style.display='none';
+
+
+
+}
+
+function showc() {
+
+document.getElementById('bota').style.display='none';
+document.getElementById('botb').style.display='none';
+document.getElementById('botc').style.display='block';
+document.getElementById('botd').style.display='none';
+
+
+
+}
+
+
+function showd() {
+
+document.getElementById('bota').style.display='none';
+document.getElementById('botb').style.display='none';
+document.getElementById('botc').style.display='none';
+document.getElementById('botd').style.display='block';
+
+
+}
